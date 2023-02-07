763e757334007503e6e8772eabf8541cf1acba3a
วรรณศักดิ์ หนูปลอด <you007543@gmail.com>
2023-02-07 23:47:26 +0700

    เพิ่มระยะการสุ่มเพื่อให้กระจายตัวมากขึ้น

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def random_bom(rows: int, cols: int, bom_number: int) -> list:
     for row in range(rows):
         row_data = []
         for col in range(cols):
-            bomb = random.randint(0, 1)
+            bomb = random.randint(0, 2)
             if bomb == 1 and bom_number > 0:
                 if sum(row_data) <= -2:
                     row_data.append(0)
